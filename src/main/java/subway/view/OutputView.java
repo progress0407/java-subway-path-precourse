ec19517bc53d4ca5fb5672bfabf3a6c7b548cbd9
swcho <progress0407@gmail.com>
2021-12-18 00:03:05 +0900

    feat: 출력부 구현

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -4,7 +4,28 @@ import subway.dto.MoveDto;
 import subway.dto.ResultDto;
 
 public class OutputView {
+
+	public static final String OUTPUT_RESULT = "## 조회 결과";
+	public static final String INFO_PREFIX_MESSAGE = "[INFO] ";
+	public static final String INFO_LINE = INFO_PREFIX_MESSAGE + "---";
+	public static final String OUTPUT_DISTANCE = INFO_PREFIX_MESSAGE + "총 거리: %dkm\n";
+	public static final String OUTPUT_TIME = INFO_PREFIX_MESSAGE + "총 소요 시간: %d분\n";
+
+	/**
+	 * ## 조회 결과
+	 * [INFO] ---
+	 * [INFO] 총 거리: 6km
+	 * [INFO] 총 소요 시간: 14분
+	 * [INFO] ---
+	 * [INFO] 교대역
+	 * [INFO] 강남역
+	 * [INFO] 양재역
+	 */
 	public static void printResult(ResultDto resultDto) {
-		System.out.println("moveDto = " + resultDto);
+		System.out.println(OUTPUT_RESULT);
+		System.out.println(INFO_LINE);
+		System.out.printf(OUTPUT_DISTANCE, resultDto.getShortestDistance());
+		System.out.printf(OUTPUT_TIME, resultDto.getShortestTime());
+		System.out.println(INFO_LINE);
 	}
 }
